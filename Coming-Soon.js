63f3bd59cfad53093c348d56dc08d92182cde6df
Christopher Elliot <untrappedltd@gmail.com>
2020-08-20 10:41:38 +0100

    Smooth Scroll Speed

--- Coming-Soon.js
+++ Coming-Soon.js
@@ -83,10 +83,10 @@ $('textarea').keyup(function() {
         var hash = this.hash;
   
         // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        // The optional number (500) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
           scrollTop: $(hash).offset().top
-        }, 800, function(){
+        }, 500, function(){
   
           // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
